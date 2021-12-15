861394a4ccbe213bb17c726c0fc5c5b4121fc501
marcus1337 <blurgas@hotmail.com>
2021-12-15 19:36:18 +0100

    framebuffer

--- src/Window/Framebuffer.cpp
+++ src/Window/Framebuffer.cpp
@@ -1,6 +1,27 @@
 #include "Framebuffer.h"
 
-
-Framebuffer::Framebuffer(int _width, int _height) : width(_width), height(_height){
-
+Framebuffer::Framebuffer(int _width, int _height) : width(_width), height(_height)
+{
 }
+
+void Framebuffer::draw(std::function<void(void)> &drawFunc, int windowWidth, int windowHeight)
+{
+    begin();
+    drawFunc();
+    end(windowWidth, windowHeight);
+    drawFramebuffer();
+}
+
+void Framebuffer::begin()
+{
+    glViewport(0, 0, width, height);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+}
+void Framebuffer::end(int windowWidth, int windowHeight)
+{
+    glViewport(0, 0, windowWidth, windowHeight);
+}
+
+ void Framebuffer::drawFramebuffer(){
+
+ }
--- src/Window/Framebuffer.h
+++ src/Window/Framebuffer.h
@@ -1,4 +1,6 @@
 #include <GL/glew.h>
+#include <functional>
+#include "ImageUniform.h"
 
 #ifndef FRAMEBUFFER_H
 #define FRAMEBUFFER_H
@@ -6,11 +8,15 @@
 class Framebuffer
 {
 
-    GLuint texture;
+    GLuint texture, fbo;
+    void begin();
+    void end(int windowWidth, int windowHeight);
+    void drawFramebuffer();
 
 public:
     const int width, height;
     Framebuffer(int _width, int _height);
+    void draw(std::function<void(void)>& drawFunc, int windowWidth, int windowHeight);
 
 };
 
