bc1dfde62e2e3c96cc39f554a32730cc04c295ee
marcus1337 <blurgas@hotmail.com>
2022-04-09 17:19:00 +0200

    font buffer size fix

--- res/shaders/text.frag
+++ res/shaders/text.frag
@@ -10,6 +10,6 @@ uniform vec4 textColor;
 
 void main()
 {    
-    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords/4.0).r);
+    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
     color = textColor * sampled;
 }  
--- src/Graphics/TextObject.cpp
+++ src/Graphics/TextObject.cpp
@@ -43,16 +43,24 @@ unsigned int TextObject::makeGlyphTexture(FT_Face &face)
     unsigned int texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
+
+    int bitWidth = face->glyph->bitmap.width;
+    int bitHeight = face->glyph->bitmap.rows;
+    unsigned char* tmpBuffer = new unsigned char[bitWidth*bitHeight*3];
+    for (int i = 0; i < bitWidth * bitHeight; i++) {
+        tmpBuffer[i*3] = face->glyph->bitmap.buffer[i];
+    }
     glTexImage2D(
         GL_TEXTURE_2D,
         0,
-        GL_RGBA,
+        GL_RGB,
         face->glyph->bitmap.width,
         face->glyph->bitmap.rows,
         0,
-        GL_RGBA,
+        GL_RGB,
         GL_UNSIGNED_BYTE,
-        face->glyph->bitmap.buffer);
+        tmpBuffer);
+    delete[] tmpBuffer;
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
