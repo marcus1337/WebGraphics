416df0aad87e98652695da7bdf1ad90e5b7a2e71
marcus1337 <blurgas@hotmail.com>
2022-06-16 14:39:46 +0200

    lobby browser

--- src/App/Panels/LobbyBrowser.cpp
+++ src/App/Panels/LobbyBrowser.cpp
@@ -27,12 +27,13 @@ LobbyButton LobbyBrowser::makeLobbyButton(NetID<int> lobbyID) {
     lbutton.button.onPressCallback = [&]() {
         std::cout << "lobby button press...\n";
     };
-    lbutton.background.texture = "background1.png";
+    lbutton.background.texture = "background3.png";
     lbutton.description.text = "Host: " + lobbyNet.getUsername(lobbyNet.getLobbyHost());
     lbutton.button.setSize(100, 60);
-    lbutton.background.width = 800;
+    lbutton.background.width = 1400;
     lbutton.background.height = 100;
     lbutton.description.pixelHeight = 30;
+    lbutton.description.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
     lbutton.button.text.text = "JOIN";
     return lbutton;
 }
@@ -43,18 +44,20 @@ void LobbyBrowser::setLobbyButtons() {
     for (NetID<int> lobbyID : lobbyNet.getOpenLobbies()) {
         LobbyButton lbutton = makeLobbyButton(lobbyID);
         
-        int xPos = 1250;
-        int yPos = 960 - (lobbyIndex * 150);
+        int xPos = 1500;
+        int yPos = 700 - (lobbyIndex * 150);
 
         lbutton.button.setPosition(xPos, yPos);
-        lbutton.background.x = xPos - lbutton.background.width + lbutton.button.image.width*2;
+        lbutton.button.text.x -= 5;
+        lbutton.button.text.y += 3;
+        lbutton.background.x = (1920 - lbutton.background.width)/2;
         lbutton.background.y = yPos - lbutton.button.image.height/3;
         lbutton.description.x = xPos - lbutton.background.width/2;
         lbutton.description.y = yPos + lbutton.description.pixelHeight/2;
 
         lobbyButtons.push_back(lbutton);
         lobbyIndex++;
-        if (lobbyIndex >= 6)
+        if (lobbyIndex >= 4)
             return;
     }
 }
@@ -83,6 +86,21 @@ void LobbyBrowser::render() {
         engine.graphics.drawImage(lbutton.background);
         engine.graphics.drawText(lbutton.description);
         renderButton(lbutton.button);
+    }
+    Text title;
+    title.text = "Lobbies";
+    title.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    title.x = 850;
+    title.y = 900;
+    engine.graphics.drawText(title);
+
+    if (lobbyButtons.empty()) {
+        Text noLobbyText;
+        noLobbyText.text = "No lobby found.";
+        noLobbyText.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+        noLobbyText.x = 700;
+        noLobbyText.y = 600;
+        engine.graphics.drawText(noLobbyText);
     }
 }
 
