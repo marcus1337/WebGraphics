0b0e6305fd6de7e956b293f817e79a5e072a4eab
marcus1337 <blurgas@hotmail.com>
2022-08-11 14:47:46 +0200

    cmake lua win32

--- src/Main.cpp
+++ src/Main.cpp
@@ -20,7 +20,15 @@
     }
 #endif
 
+extern "C" {
+    #include <lua.h>
+    #include <lualib.h>
+    #include <lauxlib.h>
+}
+
 int main(int argc, char *argv[]){
+    lua_State* L = luaL_newstate();
+    lua_close(L);
     #ifdef EMSCRIPTEN
         emscripten_set_main_loop(update, 20, 1);
     #else
