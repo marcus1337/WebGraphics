00bc50af5cba0ce1fc5abb123099a4183f509e93
marcus1337 <blurgas@hotmail.com>
2022-09-10 16:40:18 +0200

    soloud

--- src/Engine/Audio/Audio.cpp
+++ src/Engine/Audio/Audio.cpp
@@ -3,52 +3,35 @@
 #include <iostream>
 
 #include <SDL2/SDL.h>
+#include "soloud/soloud.h"
+#include "soloud/soloud_speech.h"
+#include "soloud/soloud_thread.h"
+#include "soloud/soloud_openmpt.h"
+#include "soloud/soloud_wav.h"
 
-Uint32 wav_length; // length of our sample
-Uint8* wav_buffer; // buffer containing our audio file
-SDL_AudioSpec wav_spec;
-
-Uint8* audio_pos; // global pointer to the audio buffer to be played
-Uint32 audio_len; // remaining length of the sample we have to play
-
-void my_audio_callback(void* userdata, Uint8* stream, int len) {
-
-	if (audio_len == 0)
-		return;
-
-	len = (len > audio_len ? audio_len : len);
-	SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
-
-	audio_pos += len;
-	audio_len -= len;
-}
+SoLoud::Soloud soloud;
+SoLoud::Speech speech;
+SoLoud::Wav wav;
 
 Audio::Audio() {
     if (SDL_Init(SDL_INIT_AUDIO) < 0)
         std::cout << "Failed to load SDL_INIT\n";
 
-    if (SDL_LoadWAV(getMusicFilePath("sample").c_str(), &wav_spec, &wav_buffer, &wav_length) == NULL) {
-        return;
-    }
+    speech.setText("1 2 3   1 2 3   Hello world. Welcome to So-Loud.");
+    soloud.init();
+    //soloud.play(speech);
 
-    wav_spec.callback = my_audio_callback;
-    wav_spec.userdata = NULL;
-    // set our global static variables
-    audio_pos = wav_buffer; // copy sound buffer
-    audio_len = wav_length; // copy file length
-
-    if (SDL_OpenAudio(&wav_spec, NULL) < 0) {
-        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
-        exit(-1);
-    }
-
-    SDL_PauseAudio(0);
-
+    wav.load(getMusicFilePath("sample").c_str());
+    wav.setLooping(1);                        
+    int handle1 = soloud.play(wav);           
+    soloud.setVolume(handle1, 0.5f);           
+    soloud.setPan(handle1, -0.2f);
+    soloud.setRelativePlaySpeed(handle1, 0.6f); 
 }
 
 Audio::~Audio() {
+    soloud.deinit();
     SDL_CloseAudio();
-    SDL_FreeWAV(wav_buffer);
     SDL_Quit();
 }
 
