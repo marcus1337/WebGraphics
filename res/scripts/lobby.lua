3426d40a29de8e5ae5a1f9a9b8d81683688a5d38
marcus1337 <blurgas@hotmail.com>
2022-09-17 15:05:57 +0200

    seat

--- res/scripts/lobby.lua
+++ res/scripts/lobby.lua
@@ -1,7 +1,69 @@
+Seat = {}
+Seat.__index = Seat
 
-titleText = TitleText:new{textStr = "Lobby"}
+function Seat:getTeamBackgroundColor(team)
+    if team == 0 then
+        return vec3(0.05, 0.35, 0.55)
+    else
+        return vec3(0.55, 0.35, 0.05)
+    end
+end
 
-setUIElements{getCancelButton(), getSettingsButton()}
+function Seat:getTeamBackground2Color(team)
+    if team == 0 then
+        return vec3(0.15, 0.15, 0.25)
+    else
+        return vec3(0.25, 0.15, 0.15)
+    end
+end
+
+function Seat:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    local x = o.x or 0
+    local y = o.y or 0
+    local width = 600
+    local height = 150
+    local team = o.team or 0
+    self.background = Rect.new()
+    self.background:setSize(width, height)
+    self.background:setPosition(x, y)
+    self.background:setRadius(0.3)
+    self.background:setColor(self:getTeamBackgroundColor(team))
+
+    self.background2 = Rect.new()
+    self.background2:setSize(width - 25, height - 25)
+    self.background2:setPosition(x + 12, y + 12)
+    self.background2:setRadius(0.3)
+    self.background2:setColor(self:getTeamBackground2Color(team))
+
+    self.descriptionText = Text("Goalie")
+    self.descriptionText:setColor(vec3(0.9, 0.9, 0.9))
+    self.descriptionText:setPixelHeight(50)
+    self.descriptionText:setPosition(x + 30, y + 50)
+
+    self.playerText = Text("...")
+    self.playerText:setColor(vec3(0.9, 0.9, 0.9))
+    self.playerText:setPixelHeight(50)
+    self.playerText:setPosition(x + width - self.playerText:getPixelWidth() - 50, y + 50)
+    return o
+end
+
+function Seat:update()
+
+end
+
+function Seat:render()
+    self.background:render()
+    self.background2:render()
+    self.descriptionText:render()
+    self.playerText:render()
+end
+
+titleText = TitleText:new { textStr = "Lobby" }
+setUIElements { getCancelButton(), getSettingsButton() }
+
+seat1 = Seat:new {}
 
 function update()
     updateUIElements()
@@ -10,4 +72,5 @@ end
 function render()
     renderUIElements()
     titleText:render()
+    seat1:render()
 end
--- res/scripts/main_menu.lua
+++ res/scripts/main_menu.lua
@@ -2,11 +2,14 @@
 
 backgroundImage = getBackgroundImage("background3.png")
 
-lobbyButton = MenuButton:new{btnText = "Lobby", onPressCallback = onLobby, y = 600}
-exitButton = MenuButton:new{btnText = "Exit", onPressCallback = onQuit, y = 400}
+gameButton = MenuButton:new{btnText = "Game", onPressCallback = onGame, y = 600}
+createLobbyButton = MenuButton:new{btnText = "Create Lobby", onPressCallback = onLobby, y = 500}
+findLobbyButton = MenuButton:new{btnText = "Find Lobby", onPressCallback = onFindLobby, y = 400}
+instructionsButton = MenuButton:new{btnText = "Instructions", onPressCallback = onInstructions, y = 300}
+exitButton = MenuButton:new{btnText = "Exit", onPressCallback = onQuit, y = 200}
 titleText = TitleText:new{textStr = "Menu"}
 
-setUIElements{exitButton, getSettingsButton(), lobbyButton}
+setUIElements{exitButton, getSettingsButton(), createLobbyButton, findLobbyButton, instructionsButton, gameButton}
 
 function update()
     updateUIElements()
