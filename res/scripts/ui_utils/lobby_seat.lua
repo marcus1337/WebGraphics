37829b8de44da48f82fe47afc14db272c7dc4cd5
marcus1337 <blurgas@hotmail.com>
2022-09-18 15:49:04 +0200

    refactor script

--- res/scripts/ui_utils/lobby_seat.lua
+++ res/scripts/ui_utils/lobby_seat.lua
@@ -1,5 +1,23 @@
-Seat = {}
-Seat.__index = Seat
+SimpleButton = {x = 0, y = 0, width = 700, height = 150}
+
+function SimpleButton:update()
+end
+function SimpleButton:paintView()
+end
+function SimpleButton:render()
+    if self.view == nil then
+        return
+    end
+    local view = self.view
+    view:setPosition(self.x, self.y)
+    if isMousePointerInside(view:getX(), view:getY(), view:getWidth(), view:getHeight()) then
+
+    end
+    view:render()
+end
+
+
+Seat = SimpleButton
 
 function Seat:getTeamBackgroundColor(team)
     if team == 2 then
@@ -20,52 +38,54 @@ end
 function Seat:new(o)
     o = o or {}
     setmetatable(o, self)
-    o.x = o.x or 0
-    o.y = o.y or 0
-    o.width = o.width or 700
-    o.height = o.height or 150
+    self.__index = self
     o.team = o.team or 1
     o.role = o.role or "???"
 
-    o.background = Rect.new()
-    o.background = o.background
-    o.background:setSize(o.width, o.height)
-    o.background:setRadius(0.3)
-    o.background:setColor(self:getTeamBackgroundColor(o.team))
-
-    o.background2 = Rect.new()
-    o.background2:setSize(o.width - 25, o.height - 25)
-    o.background2:setPosition(12, 12)
-    o.background2:setRadius(0.3)
-    o.background2:setColor(self:getTeamBackground2Color(o.team))
-
-    o.descriptionText = Text(o.role)
-    o.descriptionText:setColor(vec3(0.9, 0.9, 0.9))
-    o.descriptionText:setPixelHeight(50)
-    o.descriptionText:setPosition(30, 50)
-
-    o.playerText = Text("...")
-    o.playerText:setColor(vec3(0.9, 0.9, 0.9))
-    o.playerText:setPixelHeight(50)
-    o.playerText:setPosition(o.width - o.playerText:getPixelWidth() - 50, 50)
+    o:setViewComponents()
+    o.view = View.new(self.width, self.height)
+    o:paintView()
     return o
 end
 
-function Seat:update()
+function Seat:setViewComponents()
+    local background = Rect.new()
+    local width = self.width
+    local height = self.height
+    background:setSize(width, height)
+    background:setRadius(0.3)
+    background:setColor(self:getTeamBackgroundColor(self.team))
 
+    local background2 = Rect.new()
+    background2:setSize(width - 25, height - 25)
+    background2:setPosition(12, 12)
+    background2:setRadius(0.3)
+    background2:setColor(self:getTeamBackground2Color(self.team))
+
+    local descriptionText = Text(self.role)
+    descriptionText:setColor(vec3(0.9, 0.9, 0.9))
+    descriptionText:setPixelHeight(50)
+    descriptionText:setPosition(30, 50)
+
+    local playerText = Text("...")
+    playerText:setColor(vec3(0.9, 0.9, 0.9))
+    playerText:setPixelHeight(50)
+    playerText:setPosition(width - playerText:getPixelWidth() - 50, 50)
+
+    self.background = background
+    self.background2 = background2
+    self.descriptionText = descriptionText
+    self.playerText = playerText
 end
 
-function Seat:render()
-    local view = View.new(self.width, self.height)
+function Seat:paintView()
+    local view = self.view
     view:setPosition(self.x, self.y)
     view:paint(self.background)
     view:paint(self.background2)
     view:paint(self.descriptionText)
     view:paint(self.playerText)
-
-    if isMousePointerInside(view:getX(), view:getY(), view:getWidth(), view:getHeight()) then
-        print("Role: " .. self.role .. " Team: " .. tostring(self.team))
-    end
-
-    view:render()
 end
+
+
+
