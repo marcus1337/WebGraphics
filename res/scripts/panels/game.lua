6c2771ea54a4e6ace9d0fce3be685469215c4dfd
marcus1337 <blurgas@hotmail.com>
2022-11-22 12:50:10 +0100

    view position fix

--- res/scripts/panels/game.lua
+++ res/scripts/panels/game.lua
@@ -4,14 +4,12 @@ elements = Elements:new{list = {miniBtnPanel}}
 
 
 local move = Move:new()
+local board = Board:new{move = move}
 
 local promoteWidth = 115 * 4
 local promoteXPosition = math.floor(board.x + board.width / 2.0 - promoteWidth / 2.0)
 local promoteYPosition = math.floor (board.y + 115 * 8 + 10.0) 
 local promoteView = PromoteView:new{x = promoteXPosition, y = promoteYPosition, width = promoteWidth, move = move}
-
-local board = Board:new{move = move}
-
 
 function update()
     elements:update()
