1f6fa9169d381e26302e0cced2a55160613f09af
marcus1337 <blurgas@hotmail.com>
2022-12-18 15:59:14 +0100

    filepath class

---
+++ src/IO/Files/FilePath.cpp
@@ -0,0 +1,44 @@
+#include "FilePath.h"
+
+
+FilePath::FilePath(std::string _path) : path(_path) {
+
+}
+
+FileType FilePath::getType() {
+    std::string extension = getExtension();
+    if (extensionMap.contains(extension))
+        return extensionMap[extension];
+    return FileType::UNKNOWN;
+}
+
+std::string FilePath::getExtension() {
+    size_t dotIndex = path.find_last_of('.');
+    if (dotIndex != std::string::npos) {
+        return path.substr(dotIndex);
+    }
+    return "";
+}
+
+std::string FilePath::getName() {
+    std::string name = path;
+    size_t slashIndex = name.find_last_of("/\\");
+    if (slashIndex != std::string::npos)
+        name = name.substr(slashIndex + 1);
+    name = name.substr(name.length() - getExtension().length());
+    return name;
+}
+
+std::string FilePath::getPath() {
+    return path;
+}
+
+std::map<std::string, FileType> FilePath::extensionMap = {
+    {".bin", FileType::BIN},
+    {".ttf", FileType::TTF},
+    {".png", FileType::PNG},
+    {".wav", FileType::WAV},
+    {".txt", FileType::TXT},
+    {".vert", FileType::SHADER},
+    {".frag", FileType::SHADER}
+};
---
+++ src/IO/Files/FilePathContainer.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+#ifndef FILEPATHCONTAINER_H
+#define FILEPATHCONTAINER_H
+
+class FilePathContainer {
+
+public:
+
+
+};
+
+#endif
