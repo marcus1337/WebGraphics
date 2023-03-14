02d2927798eff407c6afcf4ccf5adc76f6863a93
marcus1337 <blurgas@hotmail.com>
2023-03-14 11:00:35 +0100

    remove bitsery

--- src/Window/CanvasSettings.h
+++ src/Window/CanvasSettings.h
@@ -1,16 +1,9 @@
 #include <string>
-#include "bitsery/adapter/stream.h"
-#include <bitsery/adapter/buffer.h>
-#include <bitsery/bitsery.h>
-#include <bitsery/traits/array.h>
-#include <bitsery/traits/vector.h>
-#include <bitsery/traits/string.h>
 
 #ifndef CANVASSETTINGS_H
 #define CANVASSETTINGS_H
 
 class CanvasSettings {
-    std::string fileName = "canvasSettings";
 
 public:
     CanvasSettings();
@@ -22,12 +15,5 @@ public:
     std::string appName = "MyApp";
 
 };
-
-template <typename S>
-void serialize(S& s, CanvasSettings& o) {
-    s.value4b(o.width);
-    s.value4b(o.height);
-    s.value1b(o.fullScreen);
-}
 
 #endif // !CANVASSETTINGS_H
